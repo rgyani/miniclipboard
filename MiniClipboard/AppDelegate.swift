7202bde62e488f8af73a61dfbd38decdb75c2a4c
ragy6581 <ravig1767>
2020-04-01 20:19:47 +0200

    Initial Commit

---
+++ MiniClipboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MiniClipboard
+//
+//  Created by Gyani, Ravi, SKY on 01.04.20.
+//  Copyright © 2020 Gyani, Ravi, SKY. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
